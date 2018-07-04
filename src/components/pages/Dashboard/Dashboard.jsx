d3a17c5ff05437d28562da1f20c241060009ad73
Loic PENNEQUIN <lpennequin@a-sis.division-savoye.ad>
2018-07-04 18:03:29 +0200

    started dashboard layout

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -9,7 +9,9 @@ import { translate }  from 'react-i18next';
 import { subscribe }  from 'react-contextual';
 import store          from '../../../store/store.js';
 
-// @translate()
+import DashboardSidebar from './DashboardSidebar/DashboardSidebar.jsx';
+
+@translate()
 @subscribe(store, s => {
     return { currentUser: s.currentUser }
 })
@@ -22,10 +24,35 @@ class Dashboard extends React.Component {
         const { t } = this.props;
         return (
 
-            <div className="container">
-                <h1 className="heading-1">Hello {this.props.currentUser.username}</h1>
-                <p>You have {this.props.currentUser.friendshipRequests.length} friend requests.</p>
-                <p>You have {this.props.currentUser.recievedMessages.length} unread messages.</p>
+            <div className="dashboard">
+                <DashboardSidebar />
+                <main class="dashboard_content">
+                    <h1 className="heading-1">Hello {this.props.currentUser.username}</h1>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="heading-3">Notifications widget</h3>
+                            <p>You have {this.props.currentUser.friendshipRequests.length} friend requests.</p>
+                            <p>You have {this.props.currentUser.recievedMessages.length} unread messages.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="heading-3">Apps Manager widget</h3>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="heading-3">Project Manager widget</h3>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="heading-3">FriendList Manager widget</h3>
+                        </div>
+                    </div>
+                    <button>Edit Dashboard widgets</button>
+                    <button>Edit Layout</button>
+                </main>
             </div>
 
         )
---
+++ src/components/pages/Dashboard/DashboardSidebar/DashboardSidebar.jsx
@@ -0,0 +1,32 @@
+/**
+ * Dashboard page component.
+ *
+ * @author Daria <lo.pennequin@gmail.com>
+ */
+
+import React          from 'react';
+import { translate }  from 'react-i18next';
+import { subscribe }  from 'react-contextual';
+
+@translate()
+class DashboardSidebar extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        const { t } = this.props;
+        return (
+            <div>
+                <ul>
+                    <li>FriendList</li>
+                    <li>Projects</li>
+                    <li>Apps</li>
+                    <li>Settings</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default DashboardSidebar;
